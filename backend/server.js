0aefeab470c73966227f02e28a6b90c74aa70ef5
chaitanya1-stack <ck758779@gmail.com>
2025-07-02 11:34:27 +0530

    organised the code

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-
-// server.js
-const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
+
 
 dotenv.config();
 
@@ -11,11 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Import routes
+
 const chatbotRoute = require('./routes/chatBot');
 app.use('/api/chatbotexpress', chatbotRoute);
 
-// Start server
+
 const PORT = process.env.PORT2 || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Express server running on port ${PORT}`);
